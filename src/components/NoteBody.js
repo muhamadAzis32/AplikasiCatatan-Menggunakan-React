c67a0ec318aa824380be65ce2f4da6b7c9649c52
muhamadazis32 <muhamadazis9a@gmail.com>
2022-06-02 14:11:04 +0700

    Fitur input Menambahkan Catatan

--- src/components/NoteBody.js
+++ src/components/NoteBody.js
@@ -5,7 +5,7 @@ import NoteList from "./NoteList";
 function NoteBody({ notesActive, notesArsip, setNotes }) {
     return (
         <div className="note-app__body">
-            <NoteInput />
+            <NoteInput setNotes={setNotes} />
             <NoteList label="Catatan Aktif" setNotes={setNotes} notes={notesActive} />
             <NoteList label="Arsip" setNotes={setNotes} notes={notesArsip} />
         </div>
--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -1,24 +1,45 @@
-import React from "react";
+import { useState } from "react";
+import Input from "./Input";
 
-function NoteInput() {
+function NoteInput({ setNotes }) {
+
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+    const onSubmitEventHandler = (event) => {
+        event.preventDefault();
+        const timestamp = new Date().toISOString();
+
+        setNotes((notes) => [
+            ...notes,
+            { id: timestamp, title: title, body: body, archived: false, createdAt: timestamp },
+        ]);
+    };
+
+
     return (
         <div className="note-input">
             <h2>Buat catatan</h2>
-            <form>
-                <p className="note-input__title__char-limit">Sisa karakter: 49</p>
-                <input
-                    className="note-input__title"
+            <form onSubmit={onSubmitEventHandler}>
+                <p className="note-input__title__char-limit">Sisa karakter: {50 - title.length}</p>
+                <Input
+                    value={title}
+                    onChange={setTitle}
+                    id='title'
+                    name='title'
                     type="text"
                     placeholder="Ini adalah judul ..."
                     required
                 />
-                <textarea
-                    className="note-input__body"
-                    type="text"
+                <Input
+                    value={body}
+                    onChange={setBody}
+                    id="body"
+                    name="body"
+                    type='textarea'
                     placeholder="Tuliskan catatanmu di sini ..."
                     required
-                >
-                </textarea>
+                />
                 <button type="submit">Buat</button>
             </form>
         </div>
